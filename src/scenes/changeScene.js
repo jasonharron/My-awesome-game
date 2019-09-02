837edb67043cd25e6d1e8668e4caec74d59d5560
Jason Harron <jasonharron@utexas.edu>
2019-09-02 15:03:00 -0500

    Simplified code for changeScene event listener

--- src/scenes/changeScene.js
+++ src/scenes/changeScene.js
@@ -3,79 +3,68 @@ export { addChangeSceneEventListeners };
 function addChangeSceneEventListeners(that) {
   that.input.keyboard.on(
     "keydown_ZERO",
-    function(event) {
+    function() {
       that.scene.start("Scene0");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_ONE",
-    function(event) {
+    function() {
       that.scene.start("Scene1");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_TWO",
-    function(event) {
+    function() {
       that.scene.start("Scene2");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_THREE",
-    function(event) {
+    function() {
       that.scene.start("Scene3");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_FOUR",
-    function(event) {
+    function() {
       that.scene.start("Scene4");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_FIVE",
-    function(event) {
+    function() {
       that.scene.start("Scene5");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_SIX",
-    function(event) {
+    function() {
       that.scene.start("Scene6");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_SEVEN",
-    function(event) {
+    function() {
       that.scene.start("Scene7");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_EIGHT",
-    function(event) {
+    function() {
       that.scene.start("Scene8");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_NINE",
-    function(event) {
+    function() {
       that.scene.start("Scene9");
-    },
-    that
+    }
   );
   that.input.keyboard.on(
     "keydown_ESC",
-    function(event) {
+    function() {
       that.scene.start("Boot");
-    },
-    that
+    }
   );
 }
